2e5f38b651bc49d8dfd51d2be177ec8de9f802f9
lukoq <kinderlukasz01@gmail.com>
2024-04-19 11:51:29 +0200

    Added list of items

--- App.js
+++ App.js
@@ -1,15 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import Box from '@mui/material/Box'
 import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
 import Grid from '@mui/material/Unstable_Grid2';
 import Avatar from '@mui/material/Avatar';
-import { deepOrange, yellow } from '@mui/material/colors';
+import { yellow } from '@mui/material/colors';
+import { Box, TextField, Button, List, ListItem, ListItemText, Typography } from '@mui/material';
+
+const ariaLabel = { 'aria-label': 'description' };
 
 
 function BasicCard(){
@@ -36,63 +34,124 @@ const PointCard = ({ value }) => {
 
 
 export default function App() {
+
+  const [inputText, setInputText] = React.useState('');
+  const [items, setItems] = React.useState([]);
+
+  const handleChange = (event) => {
+    setInputText(event.target.value);
+  };
+
+  const handleAddItem = () => {
+    if (inputText.trim()) { // Check if input is not empty
+      setItems([...items, inputText]);
+      setInputText(''); 
+    }
+  };
+
+
+
+
+
   return (
     <View style={styles.container}>
-      <Box 
-      maxWidth={500} // Set a maximum width of 500px
-      maxHeight={800}
-      >
-      <Grid container spacing={2}>
-        <Grid xs={3}>
-          <PointCard value={2} />
-        </Grid>
-        <Grid xs={3}>
-         <PointCard value={3} />
-        </Grid>
-        <Grid xs={3}>
-          <PointCard value={4} />
-        </Grid>
-        <Grid xs={3}>
-          <PointCard value={5} />
-        </Grid>
-      </Grid>
-
-      <Grid container spacing={2}>
-        <Grid xs={3}>
-          <BasicCard/>
-        </Grid>
-        <Grid xs={3}>
-          <BasicCard/>
-        </Grid>
-        <Grid xs={3}>
-          <BasicCard/>
-        </Grid>
-        <Grid xs={3}>
-          <BasicCard/>
-        </Grid>
-        <Grid xs={3}>
-          <BasicCard/>
-        </Grid>
-        <Grid xs={3}>
-          <BasicCard/>
-        </Grid>
-        <Grid xs={3}>
-          <BasicCard/>
-        </Grid>
-        <Grid xs={3}>
-          <BasicCard/>
-        </Grid>
-      </Grid>
-
-      </Box>
+      <View style={styles.left}></View>
+        <Box
+          maxWidth={500} // Set a maximum width of 500px
+          maxHeight={800}
+          style={{alignSelf: 'center'}}
+          >
+          <Grid container spacing={2}>
+            <Grid xs={3}>
+              <PointCard value={2} />
+            </Grid>
+            <Grid xs={3}>
+            <PointCard value={3} />
+            </Grid>
+            <Grid xs={3}>
+              <PointCard value={4} />
+            </Grid>
+            <Grid xs={3}>
+              <PointCard value={5} />
+            </Grid>
+          </Grid>
+          <Grid container spacing={2}>
+            <Grid xs={3}>
+              <BasicCard/>
+            </Grid>
+            <Grid xs={3}>
+              <BasicCard/>
+            </Grid>
+            <Grid xs={3}>
+              <BasicCard/>
+            </Grid>
+            <Grid xs={3}>
+              <BasicCard/>
+            </Grid>
+            <Grid xs={3}>
+              <BasicCard/>
+            </Grid>
+            <Grid xs={3}>
+              <BasicCard/>
+            </Grid>
+            <Grid xs={3}>
+              <BasicCard/>
+            </Grid>
+            <Grid xs={3}>
+              <BasicCard/>
+            </Grid>
+          </Grid>
+          <View style={{paddingTop: 32, flexDirection: 'row', gap: 32}}> 
+            <TextField
+              label="Enter Item"
+              variant="standard"
+              value={inputText}
+              onChange={handleChange}
+              sx={{ width: '80%'}}
+            />
+            <Button
+             variant="contained"
+             sx={{ width: '20%',  borderRadius: 8}}
+             onClick={handleAddItem} 
+             disabled={!inputText.trim()}>
+              Enter
+            </Button>
+          </View>
+        </Box>
+      <View style={styles.right}>
+      <Card 
+        variant="outlined"
+        sx={{bgcolor: yellow[50], height: '100%', width: '100%'}}
+        >
+        <CardContent>
+          <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+            You have found 2 words
+          </Typography>
+          <List sx={{ width: '100%'}}>
+            {items.map((item) => (
+              <ListItem key={item}>
+                  <ListItemText primary={item} />
+              </ListItem>
+            ))}
+          </List>
+        </CardContent>
+        </Card>
       </View>
+    </View>
   )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: 'row'
+  },
+  left: {
+    flex: 1, // Takes 1/3 of the screen
+    margin: 32,
+  },
+  right: {
+    flex: 1,
+    margin: 32,
   },
 });
